c53c6599a7590311d48943bbe867b1fb30915750
Clifton <enkentaylor@yahoo.com>
2016-06-08 22:42:08 -0400

    connected!

---
+++ src/Framework/Conn.java
@@ -0,0 +1,33 @@
+package Framework;
+import java.sql.*;
+
+public class Conn {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String url="jdbc:sqlserver://reservations.database.windows.net:1433;database=Reservations;user=opprobrious@reservations;password=13ANGels!!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
+				try
+				{
+				//Loading the driver...
+				Class.forName( "com.microsoft.sqlserver.jdbc.SQLServerDriver" );
+				System.out.println("Found the driver class!");
+				}
+				catch( java.lang.ClassNotFoundException e )
+				{
+					System.out.println(e);
+				}
+
+				try
+				{
+				 Connection m_Conn = DriverManager.getConnection(url);
+				 if (m_Conn !=null){
+					 System.out.println("Successfully connected to the database!");
+				 }
+				}catch( java.sql.SQLException e ){
+					System.out.println(e);
+				}
+		}
+
+}
